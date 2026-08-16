cb15aa988d51b2a9394b996b22514622b05ee196
ef-code <111162892+EF-Code@users.noreply.github.com>
2026-08-16 15:34:22 +0100

    refactor: name canonical program metadata

---
+++ lib/program-info.ts
@@ -0,0 +1,3 @@
+export const PROGRAM_NAME = "FIBONACCI";
+export const PROGRAM_DESCRIPTION = "Canonical seven-output Tonolith demonstration";
+export const ROM_TREE_DESCRIPTION = "4-WAY MERKLE TREE";
